de14a100035e9daa4bbe0f856e3042f51b5fbe10
Erik Forsberg <erikforsberg95@hotmail.com>
2018-11-09 18:39:11 +0100

    fixed ConnectionMatrix

---
+++ ConnectionMatrix.m
@@ -0,0 +1,38 @@
+function connectionMatrix = ConnectionMatrix(points, canvasSize)
+
+nrOfPts = length(points);
+
+matrix = zeros(canvasSize^2, 2);
+
+a = 3;
+for i = 1:nrOfPts
+    
+    position = canvasSize*(points(i,2))+points(i,1);
+    
+    if (matrix(position,1) == 0)
+        
+        matrix(position,1) = i;
+        
+    elseif(matrix(position,2) == 0)
+        matrix(position,2) = i;
+    else
+        try
+            matrix(position,a) = i;
+            a = 3;
+        catch
+            matrix = [matrix zeros(canvasSize^2, 1)];
+            a = a+1;
+        end
+    end
+    
+end
+
+connectionMatrix = [];
+
+for i = 1:canvasSize^2
+    if (matrix(i,2) ~= 0)
+        connectionMatrix(end+1,:) = matrix(i,:);
+    end
+end
+
+end
